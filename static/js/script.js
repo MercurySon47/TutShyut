5c6f433aae2d18c849e00b59e44aff1501a103e8
MercurySon47 <4755e@mail.ru>
2024-09-12 21:31:10 +0700

    add js code for scroll colapse menu nav bar

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,43 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     document.querySelectorAll('.nav-link').forEach(function (element) {
+//       element.addEventListener('click', function () {
+//         let navbarToggle = document.querySelector('.navbar-toggler');
+//         if (!navbarToggle.classList.contains('collapsed')) {
+//           navbarToggle.click();
+//         }
+//       });
+//     });
+//   });
+
+// document.addEventListener('scroll', function () {
+//   let navbarCollapse = document.querySelector('.navbar-collapse');
+//   if (navbarCollapse.classList.contains('show')) {
+//     navbarCollapse.classList.remove('show');
+//   }
+// });
 document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('.nav-link').forEach(function (element) {
-      element.addEventListener('click', function () {
-        let navbarToggle = document.querySelector('.navbar-toggler');
-        if (!navbarToggle.classList.contains('collapsed')) {
-          navbarToggle.click();
-        }
-      });
-    });
+  const navbarToggle = document.querySelector('.navbar-toggler');
+  const menuLinks = document.querySelectorAll('.nav-link');
+
+  // Функция для скрытия меню
+  function hideMenu() {
+    if (!navbarToggle.classList.contains('collapsed')) {
+      navbarToggle.click();
+    }
+  }
+
+  // Обработчик клика по ссылке в меню
+  menuLinks.forEach(function (element) {
+    element.addEventListener('click', hideMenu);
   });
+
+  // Обработчик скролла страницы
+  window.addEventListener('scroll', function () {
+    const scrollPosition = window.scrollY;
+    const scrollThreshold = 100; // Пороговое значение скролла (в пикселях)
+
+    if (scrollPosition >= scrollThreshold) {
+      hideMenu();
+    }
+  });
+});
